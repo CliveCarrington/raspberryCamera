2a798335ed51ebe5cdbcb541fcadf4ec9d56cc16
Clive CArrington <clive.carrington@btinternet.com>
2017-08-29 21:42:24 +0000

    Installed on camPi21 and changed to using AWS S3

--- bin/startCamera.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/dropbox/bin
-
-date >> ./log/images.log
-
-echo "Starting Camera Capture"
-python main.py >> ./log/images.log&
-
--- bin/statusCamera.sh
+++ bin/statusCamera.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-cd /home/pi/dropbox/bin
+cd ~/Code/raspberryCamera/bin
 
 echo "Current Activity"
 
-ps -ef | grep main.py
+ps -ef | grep piCamera.py
 tail ./log/images.log
 ls -lrt ./images | tail -5
+
+### Checking run status
+status=`/bin/ps -ef | grep piCamera.py | wc -l`
+
+if [ $status -eq "1" ] 
+then
+	echo "Need to start"
+	./startPiCamera.sh >./log/images.log &
+fi
 
 
 
